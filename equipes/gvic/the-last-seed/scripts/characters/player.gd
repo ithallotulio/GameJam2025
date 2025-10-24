c4ccf1ef8e118f10904d679aedd67082e3088625
Ithallo Tulio <51161234+ithallotulio@users.noreply.github.com>
2025-10-24 02:47:13 -0300

    Alpha 1.2
    Fix:
    - Entidade Ancestral delta parameter
    
    Add:
    - Interactions
    - Fatec Interior

--- equipes/gvic/the-last-seed/scripts/characters/player.gd
+++ equipes/gvic/the-last-seed/scripts/characters/player.gd
@@ -12,7 +12,7 @@ var stamina = 400
 var recovering = 0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
 			
